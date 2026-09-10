897fd06b6c14a390df0fae29e946ef7aef3b2279
Docente ITM <docenteitm@salasfr.itm.edu.co>
2026-09-09 21:19:36 -0500

    organizar dennis

--- Menu.java
+++ Menu.java
@@ -11,22 +11,32 @@ public class Menu {
             System.out.println("Bienvenidos compraventa Estructura de datos");
             System.out.println("Que desea realizar");
             System.out.println("1) Registro de vehiculo (Carro - moto - camioneta) ");
-            System.out.println("2) Mostrar registro ");
-            System.out.println("3) Venta de vehiculo registrado en la pila");
-            System.out.println("4) Salir ");
+            System.out.println("2) Mostrar Vehiculos Disponibles ");
+            System.out.println("3) Mostrar Vehiculos Vendidos ");
+            System.out.println("4) Mostrar Vehiculos  ");
+            System.out.println("5) Venta de vehiculo registrado en la pila");
+            System.out.println("6) Salir ");
             int opt = m.ValidarEentero(sc);
             switch (opt) {
                 case 1:
                     pila = m.LLenarPila(pila, sc, m);
                     break;
                 case 2:
-                    m.MostrarPilaObjetual(pila);
+                    m.MostrarPilaObjetual(pila, 0);
                     System.out.println();
-                    break;                
+                    break;
                 case 3:
-                    pila = m.EliminarRegitro(pila, sc, m);
+                    m.MostrarPilaObjetual(pila, 1);
+                    System.out.println();
                     break;
                 case 4:
+                    m.MostrarPilaObjetual(pila, 2);
+                    System.out.println();
+                    break;
+                case 5:
+                    pila = m.EliminarRegitro(pila, sc, m);
+                    break;
+                case 6:
                     System.out.println("Hasta luego");
                     continuar = false;
                     break;
